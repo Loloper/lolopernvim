4952745007e8c2cf2408f968a8cc469c4fa6167e
Oliver Crothers <loloper@hh01rhl39.stunet.aber.ac.uk>
2024-10-22 19:20:15 +0100

    another package added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,5 +13,15 @@ return {
 				modified = { enable = true },
 			})
 		end,
+	},
+	{
+	    "OXY2DEV/markview.nvim",
+	    lazy = false,      -- Recommended
+	    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+	    dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons"
+	    }
 	}
 }
