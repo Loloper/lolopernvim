3f0113f82d0662795eea1ba8aa18cc1c9e394447
Loloper <olivercrothers77@gmail.com>
2025-03-19 17:01:37 +0000

    python lsp added

--- lua/plugins/lspcomplete.lua
+++ lua/plugins/lspcomplete.lua
@@ -7,9 +7,16 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		dependencies = {'davidhalter/jedi'}, 
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "clangd","html", "harper_ls", "pylsp"}
+				ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"html", 
+				"harper_ls", --C&C# lsp
+				"pyright" --Python
+				}
 			})
 		end
 	},
@@ -17,11 +24,12 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+			--configs for each LSP
 			lspconfig.lua_ls.setup({})
 			lspconfig.clangd.setup({})
 			lspconfig.html.setup({})
 			lspconfig.harper_ls.setup({})
-			lspconfig.pylsp.setup({})
+			lspconfig.pyright.setup({})
 			vim.keymap.set('n','K',vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
