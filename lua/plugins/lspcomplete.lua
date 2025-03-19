2bdfdf69f44573bc53194ca41dadf562eda3d6be
Loloper <olivercrothers77@gmail.com>
2025-03-19 23:10:18 +0000

    lsp stuff

--- lua/plugins/lspcomplete.lua
+++ lua/plugins/lspcomplete.lua
@@ -6,14 +6,64 @@ return {
 		end
 	},
 	{
+		"VonHeikemen/lsp-zero.nvim",
+		--auto completion
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"saadparwaiz1/cmp_luasnip",
+
+		--snippets
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+		config = function()
+			local lspzero = require("lsp-zero")
+			local cmp = require("cmp")
+			lspzero.preset('recommended')
+			lspzero.setup()
+			cmp.setup({
+				sources = {
+					{name = 'nvim_lsp'},
+				},
+				snippet = {
+					expand = function(args)
+						-- You need Neovim v0.10 to use vim.snippet
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({}),
+			})
+
+			--per session binds
+			vim.api.nvim_create_autocmd('LspAttach', {
+				desc = 'LSP actions',
+				callback = function(event)
+					local opts = {buffer = event.buf}
+
+					vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+					vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+					vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+					vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+					vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+					vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+					vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+					vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+					vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+					vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+				end,
+			})
+		end
+	},
+	{
 		"williamboman/mason-lspconfig.nvim",
-		dependencies = {'davidhalter/jedi'}, 
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 				"lua_ls",
 				"clangd",
-				"html", 
+				"html",  
 				"harper_ls", --C&C# lsp
 				"pyright" --Python
 				}
@@ -24,6 +74,12 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+			local lspconfig_defaults = require('lspconfig').util.default_config
+			lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+				'force',
+				lspconfig_defaults.capabilities,
+				require('cmp_nvim_lsp').default_capabilities()
+			)
 			--configs for each LSP
 			lspconfig.lua_ls.setup({})
 			lspconfig.clangd.setup({})
