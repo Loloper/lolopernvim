af14c76cbc1a00a96c9dc585429d5e0c9d0ea19d
Loloper <olivercrothers77@gmail.com>
2025-03-24 18:13:12 +0000

    cmp

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
 vim.opt.number	= true
+vim.opt.signcolumn = 'yes'
+vim.opt.relativenumber= true
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.o.autochdir = false
+vim.o.autochdir = true
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
