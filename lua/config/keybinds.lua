9949e44af3ee5f6ae3afff4d515e0b2ea60c6160
Loloper <olivercrothers77@gmail.com>
2025-04-01 16:27:02 +0100

    modified keybinds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<C-N>", "<cmd>NvimTreeToggle<CR>", { noremap = true, silent = true })
 
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>g', "<cmd>Telescope live_grep<CR>", { desc = 'Telescope live grep', noremap = true, silent = true  })
+vim.keymap.set('n', '<leader>w', "<cmd>Telescope buffers<CR>", { desc = 'Telescope buffers', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>help', "<cmd>builtin.help_tag<CR>", { desc = 'Telescope help tags', noremap = true, silent = true })
