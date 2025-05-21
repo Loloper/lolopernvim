16c15b5e0c72101608ad8192b6e7eca241a513e0
Loloper <olivercrothers77@gmail.com>
2025-05-21 14:15:59 +0100

    smart selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,15 @@ return {
 				enable = true,              -- Enable highlighting
 				additional_vim_regex_highlighting = true,
 			},
+			incremental_selection = {
+				enable= true,
+				keymaps = {
+					init_selection = "<Leader>v", --start smart selection
+					node_incremental = "<Leader>vk", --increment smart selection
+					scope_incremental = "<Leader>va",
+					node_decremental = "<Leader>vj", 
+				}
+			}
 		})
 	end
   },
