1c55e172e44b63aab3bcde873c6422f219483c03
Loloper <olivercrothers77@gmail.com>
2025-05-21 14:04:08 +0100

    more fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,11 +4,11 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup(
 		{
-			ensure_installed = { "c", "lua", "python", "javascript" }, -- add your languages
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query"}, -- essential parsers that have recommended
 			auto_install = true,
 			highlight = {
 				enable = true,              -- Enable highlighting
-				additional_vim_regex_highlighting = false,
+				additional_vim_regex_highlighting = true,
 			},
 		})
 	end
